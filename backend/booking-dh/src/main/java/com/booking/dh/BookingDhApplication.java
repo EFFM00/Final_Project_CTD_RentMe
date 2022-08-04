42ecf30702817bab30cbed6355389e37078a0855
Elena Fischietto <fischietto.elena@gmail.com>
2022-08-03 21:46:48 -0300

    Tabla de Categoría creada. Falta conexión a MySQL

---
+++ backend/booking-dh/src/main/java/com/booking/dh/BookingDhApplication.java
@@ -0,0 +1,13 @@
+package com.booking.dh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingDhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingDhApplication.class, args);
+	}
+
+}
---
+++ backend/booking-dh/src/test/java/com/booking/dh/BookingDhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.dh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingDhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
