8c0bc87d607954d421e2add73305c97f76a21b08
florcaico19 <florcaico19@gmail.com>
2022-08-19 13:24:59 -0300

    products done

---
+++ backend/booking-dh/src/main/java/com/booking/dh/model/Product.java
@@ -0,0 +1,78 @@
+package com.booking.dh.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private Double longitude;
+    @Column(nullable = false)
+    private Double latitude;
+    @Column(nullable = false)
+    private int price;
+
+public Product() {
+
+}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Product(String title, String description, Double longitude, Double latitude, int price){
+    this.title = title;
+    this.description = description;
+    this.id = id;
+    this.latitude = latitude;
+    this.price = price;
+
+}
+}
