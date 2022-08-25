76aaf4e269c73ac966658f765ec0ea7f74a90ca1
Elena Dev <fischietto.elena@gmail.com>
2022-08-24 23:16:57 -0300

    configurando axios request products

--- frontend/proyecto-integrador/src/services/Products.jsx
+++ frontend/proyecto-integrador/src/services/Products.jsx
@@ -2,7 +2,11 @@ import { api } from "./api/api";
 
 // funcion para obtener o listar datos de la api
 export const getProducts = async ({setProducts}) => {
-    const resp = await api.get('/categories');
+    const resp = await api.get('/products/', {
+      params: {
+        id: 1
+      }
+    });
     setProducts(resp.data);
 }
 
