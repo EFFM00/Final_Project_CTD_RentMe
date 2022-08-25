9c2ca4ace355665740beb9d63969f1b953fcfb5e
Elena Dev <fischietto.elena@gmail.com>
2022-08-25 11:45:47 -0300

    cambios realizados 0.2

--- frontend/proyecto-integrador/src/components/DetallesProducto.jsx
+++ frontend/proyecto-integrador/src/components/DetallesProducto.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../styles/DetallesProducto.css";
 import Text from "./atoms/Text";
 import Arrow from "../assets/arrow.svg";
