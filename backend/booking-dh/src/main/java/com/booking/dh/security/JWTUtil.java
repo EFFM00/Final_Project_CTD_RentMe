5a3d3b0a218948284ac141babcf9cba8e3992f63
Elena Dev <fischietto.elena@gmail.com>
2022-09-01 14:42:01 -0300

    clase para generar token implementada

--- backend/booking-dh/src/main/java/com/booking/dh/security/JWTUtil.java
+++ backend/booking-dh/src/main/java/com/booking/dh/security/JWTUtil.java
@@ -1,6 +1,20 @@
 package com.booking.dh.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Calendar;
+import java.util.Date;
+
 public class JWTUtil {
 
+    private static final String KEY = "mG9\\n2,^obBu[8n.~MpVzbB5tHnuYF<KRE/LnQrQ<q@]wQP46vo^x{3vEN?3uN/E";
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder().setSubject(userDetails.getUsername()).setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, KEY).compact();
+    }
 
 }
