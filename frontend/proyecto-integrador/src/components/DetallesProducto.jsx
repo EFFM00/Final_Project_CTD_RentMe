77eb57c09d01864fce12403ad95de45e593fb5f4
NataliaPear <nataliaperaltacosta@gmail.com>
2022-09-03 10:55:23 -0500

    Agregando carrusel de imagenes por productos

--- frontend/proyecto-integrador/src/components/DetallesProducto.jsx
+++ frontend/proyecto-integrador/src/components/DetallesProducto.jsx
@@ -10,19 +10,40 @@ import CalendarioReservas from "./molecules/CalendarioReservas";
 import { useParams } from "react-router-dom";
 import { getProductById } from "../services/Products";
 import Button from "./atoms/Button";
-import { ContenedorBoton, SeccionReserva } from "../styles/CalendarioReservaStyle";
+import {
+  ContenedorBoton,
+  SeccionReserva,
+} from "../styles/CalendarioReservaStyle";
 import Reserva from "../pages/home/Reserva";
-
+import Gallery from "react-grid-gallery";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
 
 function DetallesProducto() {
   const { id } = useParams();
   const [dataProduct, setDataProduct] = useState([]);
-  const [ showReservation, setShowReservation ] = useState(false);
+  const [showReservation, setShowReservation] = useState(false);
+  const [images, setImages] = useState([{ source: "", caption: "image" }]);
+  const [index, setIndex] = useState(-1);
 
   // funcion para obtener los detalles de un solo producto por su id
   const getProd = async () => {
     const resp = await getProductById(id);
     setDataProduct(resp);
+
+    let img = resp.images.map((item) => {
+      return {
+        src: item.url,
+        thumbnail: item.url,
+        thumbnailWidth: 320,
+        thumbnailHeight: 174,
+      };
+    });
+    setImages(img);
   };
 
   useEffect(() => {
@@ -47,99 +68,124 @@ function DetallesProducto() {
         </div>
       </header>
 
-    {
+      {showReservation === false ? (
+        <div>
+          {/* Bloque Ubicacion */}
 
-    showReservation === false ?
+          <div className="BloqueUbicacion">
+            <div className="DatosUbi">
+              <Text
+                type="p1"
+                color="secondary"
+                text={`${dataProduct?.city?.name}, ${dataProduct?.city?.country?.name}`}
+              />
+            </div>
+            <FmdGoodIcon fontSize="small" className="Ubi" />
+            <div className="Distancia">
+              <Text type="p1" color="secondary" text={dataProduct.address} />
+            </div>
+          </div>
 
-    <div>
+          {/* Compartir Favoritos */}
 
-      {/* Bloque Ubicacion */}
+          <div className="IconComp">
+            <ShareIcon />
+          </div>
+          <div className="IconFav">
+            <FavoriteBorderIcon />
+          </div>
 
-      <div className="BloqueUbicacion">
-        <div className="DatosUbi">
-          <Text
-            type="p1"
-            color="secondary"
-            text={`${dataProduct?.city?.name}, ${dataProduct?.city?.country?.name}`}
-          />
+          {/* Carrusel */}
+
+          <div className="carrusel">
+            <div
+              style={{
+                display: "block",
+                minHeight: "1px",
+                width: "100%",
+                border: "1px solid #ddd",
+                overflow: "auto",
+              }}
+            >
+              <Gallery
+                enableLightbox={false}
+                enableImageSelection={false}
+                images={images}
+                onClickThumbnail={(index) => setIndex(index)}
+              />
+            </div>
+            <Lightbox
+              open={index >= 0}
+              close={() => setIndex(-1)}
+              index={index}
+              carousel={2}
+              slides={images}
+              plugins={[Thumbnails, Zoom, Fullscreen]}
+              zoom={{
+                maxZoomPixelRatio: 3,
+              }}
+            />
+          </div>
+
+          {/* Bloque Descripcion */}
+
+          <div className="BloqueDescripcion">
+            <Text
+              type="h1"
+              color="secondary"
+              text={`Alójate en el corazón de ${dataProduct?.city?.name}`}
+            />
+            <div className="TextoDeDescripcion">
+              <Text type="p1" text={dataProduct.description} />
+            </div>
+          </div>
+
+          {/* Bloque Caracteristicas */}
+
+          <div className="TituloC">
+            <Text type="h1" color="secondary" text="¿Qué ofrece este lugar?" />
+          </div>
+          <div className="BloqueDeCaracteristicas">
+            <div className="caracteristicas">
+              <ul
+                className="listaCaracteristicas"
+                style={{ paddingLeft: "30px" }}
+              >
+                {dataProduct?.characteristicsXProducts?.map((item) => (
+                  <li className="itemCaracteristica" key={item.id}>
+                    {item?.characteristic?.description}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          {/* Bloque Calendario */}
+
+          <SeccionReserva>
+            <CalendarioReservas />
+            <ContenedorBoton>
+              <Text
+                type="h3"
+                color="tertiary"
+                text="Agregá tus fechas de viaje para obtener precios exactos"
+              />
+              <Button
+                text="Iniciar reserva"
+                fullwidth
+                click={() => setShowReservation(true)}
+              />
+            </ContenedorBoton>
+          </SeccionReserva>
         </div>
-        <FmdGoodIcon fontSize="small" className="Ubi" />
-        <div className="Distancia">
-          <Text type="p1" color="secondary" text={dataProduct.address} />
-        </div>
-      </div>
-
-      {/* Compartir Favoritos */}
-
-      <div className="IconComp">
-        <ShareIcon />
-      </div>
-      <div className="IconFav">
-        <FavoriteBorderIcon />
-      </div>
-
-      {/* Carrusel */}
-
-      <div className="carrusel">
-        <h1>Aqui van las imagenes</h1>
-        {/* {
-          dataProduct?.images?.map(image => 
-            <img key={image.id} src={image.url} alt={image.title}/>
-          )
-        } */}
-      </div>
-
-      {/* Bloque Descripcion */}
-
-      <div className="BloqueDescripcion">
-        <Text
-          type="h1"
-          color="secondary"
-          text={`Alójate en el corazón de ${dataProduct?.city?.name}`}
+      ) : (
+        <Reserva
+          mainPictureUrl={dataProduct.mainPictureUrl}
+          category={dataProduct?.category?.title}
+          title={dataProduct.title}
+          address={dataProduct.address}
         />
-        <div className="TextoDeDescripcion">
-          <Text type="p1" text={dataProduct.description} />
-        </div>
-      </div>
-
-      {/* Bloque Caracteristicas */}
-
-      <div className="TituloC">
-        <Text type="h1" color="secondary" text="¿Qué ofrece este lugar?" />
-      </div>
-      <div className="BloqueDeCaracteristicas">
-        <div className="caracteristicas">
-          <ul className="listaCaracteristicas" style={{paddingLeft: "30px"}}>
-            {
-              dataProduct?.characteristicsXProducts?.map(item =>
-                <li className="itemCaracteristica" key={item.id}>{item?.characteristic?.description}</li>
-              )
-            }
-          </ul>
-        </div> 
-      </div>
-
-      {/* Bloque Calendario */}
-
-      <SeccionReserva>
-        <CalendarioReservas/>
-        <ContenedorBoton>
-            <Text type="h3" color='tertiary' text="Agregá tus fechas de viaje para obtener precios exactos"/>
-            <Button text="Iniciar reserva" fullwidth click={() => setShowReservation(true)}/>
-        </ContenedorBoton>
-      </SeccionReserva>
-
-    </div>
-
-    : <Reserva
-        mainPictureUrl={dataProduct.mainPictureUrl}
-        category={dataProduct?.category?.title}
-        title={dataProduct.title}
-        address={dataProduct.address}
-      />
-    }
-
-    
+      )}
 
       {/* Bloque de Politicas */}
 
@@ -148,14 +194,13 @@ function DetallesProducto() {
       </div>
 
       <div className="BloqueDePoliticas">
-          <ul className="listaPoliticas" style={{paddingLeft: "30px"}}>
-            {
-              dataProduct?.policiesXProducts?.map(item =>
-                
-                <li className="itemPolitica" key={item.id}>{item?.policy?.policyType?.name} - {item?.policy?.description}</li>
-              )
-            }
-          </ul>
+        <ul className="listaPoliticas" style={{ paddingLeft: "30px" }}>
+          {dataProduct?.policiesXProducts?.map((item) => (
+            <li className="itemPolitica" key={item.id}>
+              {item?.policy?.policyType?.name} - {item?.policy?.description}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
