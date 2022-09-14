db97a21af549102ad010334c37115ff7fccb55ee
Elena Dev <fischietto.elena@gmail.com>
2022-09-14 20:29:27 -0300

    estilos otras ramas unificado. Falta trabajar en el calendario

--- frontend/proyecto-integrador/src/components/molecules/CalendarioReservas.jsx
+++ frontend/proyecto-integrador/src/components/molecules/CalendarioReservas.jsx
@@ -1,39 +1,67 @@
-import React from 'react'
-import Text from '../atoms/Text'
-import Calendar from 'react-calendar';
-import { ContainerCalendar} from '../../styles/CalendarioReservaStyle';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import React from "react";
+//import Text from "../atoms/Text";
+import Calendar from "react-calendar";
+import { ContainerCalendar } from "../../styles/CalendarioReservaStyle";
+import { useState } from "react";
+import { useEffect } from "react";
+import { getAllBookings } from "../../services/Booking";
+//import { useParams } from "react-router-dom";
 // import dayjs from "dayjs"; // ES 2015
 
-
-function CalendarioReservas() {
-    const [ tablet, setTablet ] = useState(false);
+function CalendarioReservas({ idProd }) {
+    const [tablet, setTablet] = useState(false);
     const [dateValue, setDateValue] = useState(new Date());
     const minDate = new Date();
+    //const { id } = useParams();
+    const [fechasNoDisponibles, setFechasNoDisponibles] = useState([]);
+
+    // const formatDate = (date) => {
+    //     return dayjs(date).format("DD-MM-YYYY");
+    // };
+
+    const getBookings = async () => {
+        const resp = await getAllBookings(idProd);
+
+        setFechasNoDisponibles(resp);
+    };
 
     useEffect(() => {
-        const responsive = () => window.innerWidth >= 768 ? setTablet(true) : setTablet(false)
+        getBookings();
+    });
+    console.log(fechasNoDisponibles);
+
+    useEffect(() => {
+        const responsive = () =>
+            window.innerWidth >= 768 ? setTablet(true) : setTablet(false);
         responsive();
-        window.addEventListener("resize", ()=>responsive())
-    }, [])
+        window.addEventListener("resize", () => responsive());
+    }, []);
 
-    
-  return (
-    <>
-        <Titulo>
-            <Text type="h1" color='secondary' text="Fechas disponibles"/>
-        </Titulo>
-
-        <ContainerCalendar id="calendarCont2">
-            {
-                tablet ? <Calendar showDoubleView={true} selectRange={true} minDate={minDate} onChange={(value) => setDateValue(value)} value={dateValue} id="calendarCont3"/>
-                : <Calendar showDoubleView={false} selectRange={true} minDate={minDate} onChange={(value) => setDateValue(value)}
-                value={dateValue} id="calendarCont4"/>
-            }
-        </ContainerCalendar>
-    </>
-  )
+    return (
+        <>
+            <ContainerCalendar id="calendarCont2">
+                {tablet ? (
+                    <Calendar
+                        showDoubleView={true}
+                        selectRange={true}
+                        minDate={minDate}
+                        onChange={(value) => setDateValue(value)}
+                        value={dateValue}
+                        id="calendarCont3"
+                    />
+                ) : (
+                    <Calendar
+                        showDoubleView={false}
+                        selectRange={true}
+                        minDate={minDate}
+                        onChange={(value) => setDateValue(value)}
+                        value={dateValue}
+                        id="calendarCont4"
+                    />
+                )}
+            </ContainerCalendar>
+        </>
+    );
 }
 
-export default CalendarioReservas
+export default CalendarioReservas;
