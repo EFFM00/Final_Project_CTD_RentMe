0fb9faf7da53f344d049a0f0b73e929378d30a9f
Elena Dev <fischietto.elena@gmail.com>
2022-09-21 17:18:28 -0300

    cards recomendaciones modificado

--- frontend/proyecto-integrador/src/styles/CategoriaStyle.jsx
+++ frontend/proyecto-integrador/src/styles/CategoriaStyle.jsx
@@ -7,12 +7,14 @@ export const ContenedorCtg = styled.article`
     border: 1px solid #F3F1ED;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     border-radius: 10px;
+    max-width: 500px;
 `
 
 export const Imagen = styled.img`
     width: 100%;
     height: 240px;
     border-radius: 8px 8px 0px 0px;
+    object-fit: cover;
 `
 
 export const Info = styled.div`
