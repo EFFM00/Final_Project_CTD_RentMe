1d6cc29b50e5f8874aacaa7b65c0169dc5d54c42
Elena Dev <fischietto.elena@gmail.com>
2022-09-23 13:24:10 -0300

    cursor personalizado

--- frontend/proyecto-integrador/src/styles/GlobalStyle.jsx
+++ frontend/proyecto-integrador/src/styles/GlobalStyle.jsx
@@ -9,6 +9,51 @@ export const GlobalStyle = createGlobalStyle`
 
     body{
         font-family: 'Montserrat', sans-serif;
+        cursor: none;
     }
-`
 
+    .cursor, .cursor:hover {
+        z-index: 1;
+        width: 25px;
+        height: 25px;
+        border: 5px solid #FD866E;
+        border-radius: 50%;
+        position: absolute;
+    }
+
+    @keyframes cursorAnim {
+        from {
+            transform: scale(1);
+        }
+        to {
+            transform: scale(.7);
+        }
+    }
+
+    @keyframes cursorAnim2 {
+        from {
+            transform: scale(1);
+        }
+        to {
+            transform: scale(.4);
+        }
+    }
+
+    @keyframes cursorAnim3 {
+        0% {
+            transform: scale(1);
+        }
+        50% {
+            transform: scale(3);
+        }
+        100% {
+            transform: scale(1);
+            opacity: 0;
+        }
+    }
+
+    .expand {
+        animation: cursorAnim3 .5s forwards;
+        border: 1px solid #FD866E;
+    }
+`;
