803214befda368123e88ba70f2783b9f5f3045ff
Elena Dev <fischietto.elena@gmail.com>
2022-09-23 13:37:00 -0300

    retoque final final

--- frontend/proyecto-integrador/src/styles/GlobalStyle.jsx
+++ frontend/proyecto-integrador/src/styles/GlobalStyle.jsx
@@ -9,51 +9,54 @@ export const GlobalStyle = createGlobalStyle`
 
     body{
         font-family: 'Montserrat', sans-serif;
-        cursor: none;
     }
 
-    .cursor, .cursor:hover {
-        z-index: 1;
-        width: 25px;
-        height: 25px;
-        border: 5px solid #FD866E;
-        border-radius: 50%;
-        position: absolute;
-    }
+    .cursor {
+    width: 20px;
+    height: 20px;
+    border: 3px solid #FD866E;
+    border-radius: 50%;
+    position: absolute;
+    
+    /* transition-timing-function: ease-out;
+    animation: cursorAnim .5s infinite alternate;
+    pointer-events: none; */
+}
 
-    @keyframes cursorAnim {
-        from {
-            transform: scale(1);
-        }
-        to {
-            transform: scale(.7);
-        }
+@keyframes cursorAnim {
+    from {
+        transform: scale(1);
     }
+    to {
+        transform: scale(.7);
+    }
+}
 
-    @keyframes cursorAnim2 {
-        from {
-            transform: scale(1);
-        }
-        to {
-            transform: scale(.4);
-        }
+@keyframes cursorAnim2 {
+    from {
+        transform: scale(1);
     }
+    to {
+        transform: scale(.4);
+    }
+}
 
-    @keyframes cursorAnim3 {
-        0% {
-            transform: scale(1);
-        }
-        50% {
-            transform: scale(3);
-        }
-        100% {
-            transform: scale(1);
-            opacity: 0;
-        }
+@keyframes cursorAnim3 {
+    0% {
+        transform: scale(1);
     }
+    50% {
+        transform: scale(3);
+    }
+    100% {
+        transform: scale(1);
+        opacity: 0;
+    }
+}
 
-    .expand {
-        animation: cursorAnim3 .5s forwards;
-        border: 1px solid #FD866E;
-    }
+.expand {
+    animation: cursorAnim3 .5s forwards;
+    border: 1px solid #fdab9b;
+}
+
 `;
